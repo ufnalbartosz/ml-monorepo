960ae39ea330e064445604f7a6f4e3e7b875dec5
Bartosz Ufnal <ufnalbartosz@gmail.com>
2026-08-02 13:23:10 +0200

    fix(dfp-formula-project): drop mayavi, defer its import into the unused surface method

--- packages/dfp-formula-project/src/dfp_formula_project/figure.py
+++ packages/dfp-formula-project/src/dfp_formula_project/figure.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
-from mayavi import mlab
 #matplotlib3d
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
@@ -46,6 +45,7 @@ class Figure():
 
 
     def plot_surf_mayavi(self):
+        from mayavi import mlab
         if self.fdim == 2:
             mlab.surf(self.x_range, self.y_range, self.z_val, warp_scale='auto')
             mlab.show()
